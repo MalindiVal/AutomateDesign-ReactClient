b003493a8aaa133a5344da1548ed1893331c763e
MalindiVal <63977034+MalindiVal@users.noreply.github.com>
2026-03-24 21:55:23 +0100

    Ajout de la navigation

--- app/automateviewer.tsx
+++ app/automateviewer.tsx
@@ -1,16 +1,68 @@
 import { automateDao } from "@/api/automateDao";
 import AutomateCanvas from "@/components/automateCanvas";
-import { useLocalSearchParams } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { useLocalSearchParams, useNavigation } from "expo-router";
 import { useEffect, useState } from "react";
-import { Text, View } from "react-native";
+import { Alert, Text, TouchableOpacity, View } from "react-native";
 
 export default function AutomateViewerScreen() {
-  const { id } = useLocalSearchParams<{ id?: string | string[] }>();
+  const { id } = useLocalSearchParams<{ id?: string }>();
   const [automate, setAutomate] = useState<any>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const navigation = useNavigation();
 
   useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          onPress={() =>
+            Alert.alert("Options", "Choisis une action", [
+              {
+                text: "Modifier",
+                onPress: () => console.log("Edit"),
+              },
+              {
+                text: "Supprimer",
+                onPress: () => {
+                  Alert.alert(
+                    "Confirmation",
+                    "Êtes-vous sûr de vouloir supprimer cet automate ?",
+                    [
+                      { text: "Annuler", style: "cancel" },
+                      {
+                        text: "Supprimer",
+                        style: "destructive",
+                        onPress: async () => {
+                          if (!id) return;
+
+                          await automateDao.deleteAutomate(id);
+                          console.log("Automate supprimé");
+
+                          navigation.goBack();
+                        },
+                      },
+                    ],
+                  );
+                },
+              },
+              {
+                text: "Exporter l'automate en Image",
+                onPress: () => console.log("Export as Image"),
+              },
+              { text: "Annuler", style: "cancel" },
+            ])
+          }
+        >
+          <Ionicons
+            name="ellipsis-vertical"
+            size={24}
+            style={{ marginRight: 15 }}
+          />
+        </TouchableOpacity>
+      ),
+    });
+
     const fetchAutomate = async () => {
       if (!id) {
         setLoading(false);
